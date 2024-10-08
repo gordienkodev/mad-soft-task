12adb9e28cb83c771fe26a21114ee36587fe43af
Pavel Gordienko <guzmo@yandex.ru>
2024-10-08 14:01:17 +0300

    feat: add deadline save

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -4,17 +4,16 @@ import { CountdownTimer } from '../CountdownTimer/CountdownTimer';
 import { TestDataLoader } from '../TestDataLoader';
 import { CurrentQuestion } from '../CurrentQuestion/CurrentQuestion';
 import { ProgressBar } from '../ProgressBar/ProgressBar';
+import { DeadlineTimerManager } from '../DeadlineTimerManager';
 
-const deadline = Date.now() + 1000 * 60 * 20;
-
-type QuestionType = 'single-choice' | 'multiple-choice';
+type QuestionType = 'single-choice' | 'multiple-choice' | 'short-answer';
 
 interface BaseQuestion {
   id: string;
   type: QuestionType;
   question: string;
-  options: string[];
-  correctAnswer: string;
+  options?: string[];
+  correctAnswer?: string | string[];
 }
 
 interface Test {
@@ -28,6 +27,7 @@ export const TestComponent = () => {
   const [data, setData] = useState<Test | null>(null);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedAnswers, setSelectedAnswers] = useState<{ [key: string]: string | string[] }>({});
+  const [deadline, setDeadline] = useState<number>(0);
 
   const onFinish = () => {
     console.log('finished!');
@@ -60,21 +60,23 @@ export const TestComponent = () => {
 
   return (
     <>
+      <DeadlineTimerManager timeLimit={1000 * 60 * 20} onDeadlineCalculated={setDeadline} />
+
       {!data ? (
         <TestDataLoader onDataLoad={handleDataLoad} />
       ) : (
         <>
           <Flex className="header-row" gap="middle" align="center" justify="start">
             <div>Тестирование</div>
-            <CountdownTimer deadline={deadline} onFinish={onFinish} />
+            {deadline > 0 && <CountdownTimer deadline={deadline} onFinish={onFinish} />}
           </Flex>
 
           <ProgressBar questions={data.questions} currentQuestionIndex={currentQuestionIndex} />
 
           <CurrentQuestion
             question={data.questions[currentQuestionIndex]}
-            value={(selectedAnswers[data.questions[currentQuestionIndex].id] as string[]) ?? []}
-            onChange={onChange} 
+            value={selectedAnswers[data.questions[currentQuestionIndex].id] ?? ''}
+            onChange={onChange}
             handleNextQuestion={handleNextQuestion}
           />
         </>
